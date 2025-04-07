5abd0e67deacaa338167dc3b408806dfb49a9017
Nicoll <nyacostad@udistrital.edu.co>
2025-04-07 13:09:13 -0500

    Correccion MySQL

--- src/config/db.js
+++ src/config/db.js
@@ -1,9 +1,14 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
-    host: process.env.DB_HOST,
-    dialect: 'mysql'
+const sequelize = new Sequelize(process.env.DB_URL, {
+    dialect: 'mysql',
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        }
+    }
 });
 
 sequelize.authenticate()
