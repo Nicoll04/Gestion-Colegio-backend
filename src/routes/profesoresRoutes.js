e5b4a047ae8d99ae29f70da5c2ebfecdb49796b6
Nicoll <nyacostad@udistrital.edu.co>
2025-05-16 14:04:32 -0500

    Correcciones en las rutas

--- src/routes/profesoresRoutes.js
+++ src/routes/profesoresRoutes.js
@@ -4,8 +4,8 @@ const profesoresController = require('../controllers/profesoresController');
 const { verificarToken, verificarRol } = require('../middleware/auth');
 const upload = require('../config/multer'); 
 
-router.get('/', verificarToken, verificarRol(["admin", "coordinacion", "secretaria","orientacion"]), profesoresController.getAllProfesores);
-router.get('/:id', verificarToken, verificarRol(["admin", "coordinacion", "secretaria","orientacion"]), profesoresController.getProfesorById);
+router.get('/', verificarToken, verificarRol(["admin", "coordinacion", "secretaria","orientacion","profesor"]), profesoresController.getAllProfesores);
+router.get('/:id', verificarToken, verificarRol(["admin", "coordinacion", "secretaria","orientacion","profesor"]), profesoresController.getProfesorById);
 
 router.post("/", verificarToken, verificarRol(["admin"]),upload.single("Foto"), profesoresController.createProfesor);
 router.put('/:id', verificarToken, verificarRol(["admin"]), profesoresController.updateProfesor);
