5c2aaa2b261977127979585ca247f65608a4ea51
Nicoll <nyacostad@udistrital.edu.co>
2025-05-16 13:37:57 -0500

    Correcciones en las rutas

--- src/routes/cursosRoutes.js
+++ src/routes/cursosRoutes.js
@@ -4,9 +4,9 @@ const cursosController = require('../controllers/cursosController');
 const { verificarToken, verificarRol} = require('../middleware/auth'); 
 
 
-router.get('/', verificarToken, verificarRol(["admin", "coordinacion", "secretaria","orientacion"]), cursosController.getAllCursos);
-router.get('/:id', verificarToken, verificarRol(["admin", "coordinacion", "secretaria","orientacion"]), cursosController.getCursoById);
-router.get('/:id/estudiantes', verificarToken, verificarRol(["admin", "coordinacion", "secretaria","orientacion"]), cursosController.getEstudiantesByCurso);
+router.get('/', verificarToken, verificarRol(["admin", "coordinacion", "secretaria","orientacion","profesor"]), cursosController.getAllCursos);
+router.get('/:id', verificarToken, verificarRol(["admin", "coordinacion", "secretaria","orientacion","profesor"]), cursosController.getCursoById);
+router.get('/:id/estudiantes', verificarToken, verificarRol(["admin", "coordinacion", "secretaria","orientacion","profesor"]), cursosController.getEstudiantesByCurso);
 
 router.post('/', verificarToken, verificarRol(["admin"]),cursosController.createCurso);
 router.put('/:id', verificarToken,verificarRol(["admin"]), cursosController.updateCurso);
