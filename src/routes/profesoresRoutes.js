b7866bbdc3589a527748687841cf09aaa727a3f0
Nicoll <nyacostad@udistrital.edu.co>
2025-05-20 14:10:14 -0500

    Correccion de roles

--- src/routes/profesoresRoutes.js
+++ src/routes/profesoresRoutes.js
@@ -4,13 +4,13 @@ const profesoresController = require('../controllers/profesoresController');
 const { verificarToken, verificarRol } = require('../middleware/auth');
 const upload = require('../config/multer'); 
 
-router.get('/', verificarToken, verificarRol(["admin", "coordinacion", "secretaria","orientacion"]), profesoresController.getAllProfesores);
-router.get('/:id', verificarToken, verificarRol(["admin", "coordinacion", "secretaria","orientacion"]), profesoresController.getProfesorById);
+router.get('/', verificarToken, verificarRol(["admin", "coordinacion", "secretaria","orientacion","profesor"]), profesoresController.getAllProfesores);
+router.get('/:id', verificarToken, verificarRol(["admin", "coordinacion", "secretaria","orientacion","profesor"]), profesoresController.getProfesorById);
 
 router.post("/", verificarToken, verificarRol(["admin"]),upload.single("Foto"), profesoresController.createProfesor);
 router.put('/:id', verificarToken, verificarRol(["admin"]), profesoresController.updateProfesor);
 router.delete('/:id', verificarToken, verificarRol(["admin"]),profesoresController.deleteProfesor);
-router.get('/by-correo/:correo', verificarToken, verificarRol(["admin"]), profesoresController.getProfesorByCorreo);
+router.get('/by-correo/:correo', verificarToken, verificarRol(["admin","profesor"]), profesoresController.getProfesorByCorreo);
 
 
 module.exports = router;
